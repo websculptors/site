1720d6a7a6420bfa749c1656ee6d783fc081801b
milkyskies <me@yozora.dev>
2022-08-05 05:21:57 +0900

    refactor: Change  images directory

--- elder.config.js
+++ elder.config.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
-  origin: 'https://example.com', // TODO: update this. The URL of your site's root, without a trailing slash
-  lang: 'en',
-  srcDir: 'src',
-  distDir: 'public',
+  origin: "https://example.com", // TODO: update this. The URL of your site's root, without a trailing slash
+  lang: "en",
+  srcDir: "src",
+  distDir: "public",
   rootDir: process.cwd(),
   build: {},
-  prefix: '', // If you want your site to be built within a sub folder within your `distDir` you can use this.
+  prefix: "", // If you want your site to be built within a sub folder within your `distDir` you can use this.
   server: {},
   props: {
-    hydration: 'hybrid',
+    hydration: "hybrid",
     compress: false,
   },
   debug: {
@@ -23,18 +23,27 @@ module.exports = {
     // disable: ['elderWriteHtmlFileToPublic'], // this is used to disable internal hooks. Uncomment this hook to disabled writing your files during build.
   },
   plugins: {
-    '@elderjs/plugin-markdown': {
-      routes: ['blog'],
+    "@elderjs/plugin-markdown": {
+      routes: ["blog"],
     },
-    '@elderjs/plugin-browser-reload': {
+    "@elderjs/plugin-browser-reload": {
       // this reloads your browser when nodemon restarts your server.
       port: 8080,
       reload: true, // if you are having issues with reloading not working, change to true.
     },
-    '@elderjs/plugin-seo-check': {
-      display: ['errors', 'warnings'], // If the errors are too verbose remove 'warnings'
+    "@elderjs/plugin-seo-check": {
+      display: ["errors", "warnings"], // If the errors are too verbose remove 'warnings'
       //writeLocation: './report.json', // if you want to write a report of errors
     },
+    "@elderjs/plugin-images": {
+      folders: [
+        {
+          src: "/assets/images/**/*", // glob of where your original images are. Relative to rootDir/process.cwd() defined in your elder.config.js. Careful with **.
+          // src: 'articles/**/*' is also acceptable
+          output: "/assets/images/", // where files should be put within the distDir defined in your elder.config.js.
+        },
+      ],
+    },
   },
-  shortcodes: { closePattern: '}}', openPattern: '{{' },
+  shortcodes: { closePattern: "}}", openPattern: "{{" },
 };
